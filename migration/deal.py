1e9a373fb873b6468d4f06fe894c0e2bf0345ba6
TuanNA <tuanna2.ho@mcredit.com.vn>
2021-11-04 23:11:24 +0700

    Change mapping haravan to bitrix

--- migration/deal.py
+++ migration/deal.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 def HaravanToBitrix24(ha):
     
     bx = {}    
-    bx['ID'] = ha.get('id' , 0)
+    # bx['ID'] = ha.get('id' , 0)
     bx['TITLE'] = 'Haravan Order ' + ha.get('name', 'not_found')
     bx['ADDITIONAL_INFO'] = ha.get('note', 'no information')
     bx['OPPORTUNITY'] = ha.get('total_price', 0)
@@ -12,12 +12,12 @@ def HaravanToBitrix24(ha):
     bx['DATE_CREATE'] = ha.get('created_at')[0:10]
     bx['BEGINDATE'] = ha.get('created_at')[0:10]
 
-    contact_id =  bx24.getContactIDbyPhone(ha['customer']['phone'] or ha['billing_address']['phone'])[0] or [{'ID':'0'}]['ID']
-    bx['CONTACT_ID'] = contact_id
-    bx['STAGE_ID'] = "C18:NEW"
+    # contact_id =  bx24.getContactIDbyPhone(ha['customer']['phone'] or ha['billing_address']['phone'])[0] or [{'ID':'0'}]['ID']
+    # bx['CONTACT_ID'] = contact_id
+    # bx['STAGE_ID'] = "C18:NEW"
     bx['CURRENCY_ID'] = "VND"
     bx['IS_MANUAL_OPPORTUNITY'] = "N"
-    bx['CATEGORY_ID'] = "18"
+    # bx['CATEGORY_ID'] = "18"
     bx['STAGE_SEMANTIC_ID'] = "P"
 
     print('HaravanToBitrix24',bx)
