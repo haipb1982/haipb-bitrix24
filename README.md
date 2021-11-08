52365e5793b775d413cc78e64b6d7e325e28ddfb
haipb <phan.ba.hai@rewardstyle.com>
2021-11-08 08:54:35 +0700

    deploy to blu

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ heroku logs --app haipb-bitrix24 -t
 
 outbound webhook
 https://haipb-bitrix24.herokuapp.com/api/v1/bitrix24/webhooks/deal
+
+
+# link to subcribe
+
+https://accounts.haravan.com/connect/authorize/callback?response_mode=form_post&response_type=code id_token&scope=openid profile email org userinfo&client_id=f7dfbb2bd41e37b59a279208ec7ef783&redirect_uri=https://haipb-api.herokuapp.com/install/login&nonce=haipb1982
--- bx24.py
+++ bx24.py
@@ -5,9 +5,9 @@ from datetime import datetime
 import migration.deal as Deal
 
 
-# bx24 = Bitrix24('https://blusaigon.bitrix24.com/rest/2069/pc3dgsz0s0ohfz6v/crm.deal.fields.json')
+bx24 = Bitrix24('https://blusaigon.bitrix24.com/rest/2069/pc3dgsz0s0ohfz6v/crm.deal.fields.json')
 # bx24 = Bitrix24('https://b24-hfk65b.bitrix24.com/rest/1/ppyzdjwvsgune1od/crm.deal.fields.json') # TuanNA
-bx24 = Bitrix24('https://b24-nd8219.bitrix24.vn/rest/1/cjgdujez4jbs6nch/profile.json') # HaiPB
+# bx24 = Bitrix24('https://b24-nd8219.bitrix24.vn/rest/1/cjgdujez4jbs6nch/profile.json') # HaiPB
 
 # # # # # # 
 
